6e62e256dcf82a0bfc09581305b7062ff9b2afe5
alexdevvv <alex.dev.sergeev@gmail.com>
2021-02-27 18:49:47 +0300

    4.1. Операторы ветвлений [#481036]

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-1);
+    }
+
+    public static void possibleDiv(int number){
+        if(number < 0){
+            System.out.println("This is negative numbers");
+        } else if (number == 0){
+            System.out.println("This number is 0");
+        }else {
+            System.out.println("This is positive numbers");
+        }
+    }
+}
