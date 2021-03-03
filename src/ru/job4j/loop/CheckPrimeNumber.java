015b59cb28b5212d1f04d11273d5f510fa41f322
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-03-03 12:14:51 +0300

    5.6. Простые числа [#481132] (V 1.1)

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,7 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if(number == 1){
-            prime = false;
-            return prime;
-        }else {
+        boolean prime = number > 1;
             for (int i = 2; i < number; i++){
                 if(number % i == 0){
                     prime = false;
@@ -18,4 +14,4 @@ public class CheckPrimeNumber {
         }
 
     }
-}
+
