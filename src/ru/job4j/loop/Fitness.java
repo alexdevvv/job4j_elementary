5ecd59631bd7ec6d7899b71bab2d98a01fda7100
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-03-03 12:19:18 +0300

    5.6. Простые числа [#481132] (V 1.2)

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,10 +3,7 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        if(ivan == nik){
-            return month + 1;
-        } else {
-            while (ivan < nik){
+            while (ivan <= nik){
                 ivan *= 3;
                 nik *= 2;
                 month += 1;
@@ -15,4 +12,4 @@ public class Fitness {
         }
 
     }
-}
+
