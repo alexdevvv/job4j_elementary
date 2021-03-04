4824dabc6d551684e62e6f67a0623f69b3d70112
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-03-04 14:50:17 +0300

    6.0.1. Объявление массива. [#481070]

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -9,9 +9,9 @@ public class ArrayLoop {
             array[i] = i * 2 + 3;
         }
 
-        // Выводим массив на консоль
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        // Выводим массив на консоль с помощью  for-each
+        for (Integer i : array) {
+            System.out.println(i);
         }
 
     }
