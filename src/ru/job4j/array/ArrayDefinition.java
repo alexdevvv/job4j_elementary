65ef9b7cf8f2b8699f2a15c2b47042c452817aee
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-03-04 14:40:31 +0300

    6.0.1. Объявление массива. [#481070] (V 1.2)

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,19 +6,6 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-//          String[] names = new String[4];
-//          names[0] = "Alex Matveev";
-//          names[1] = "Anton Avdeev";
-//          names[2] = "Olga Shalina";
-//          names[3] = "Oleg Makarov";
 
-//        System.out.println("Размер массива ages равен: " + ages.length);
-//        System.out.println("Размер массива surnames равен: " + surnames.length);
-//        System.out.println("Размер массива prices равен: " + prices.length);
-//
-//        System.out.println(names[0]);
-//        System.out.println(names[1]);
-//        System.out.println(names[2]);
-//        System.out.println(names[3]);
     }
 }
