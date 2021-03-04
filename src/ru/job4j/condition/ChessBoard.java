f994e1a2d8fd6f29fe74292c048e4c411181ff15
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-03-04 10:27:54 +0300

    5.9. Крест в псевдографике. [#481135]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,6 +4,7 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         if (Math.abs(x1 - x2) == Math.abs(y1 - y2) ) {
             return  Math.abs(x1 - x2);
-        } return 0;
+        }
+        return 0;
     }
 }
